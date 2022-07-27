9499d45dbe6b220eebc49ec09802b2ada931f05b
calito112 <chisomogueji@gmail.com>
2022-07-27 11:12:16 +0100

    My Bank

--- contracts/bank.sol
+++ contracts/bank.sol
@@ -1,33 +1,42 @@
-pragma solidity ^0.8.6; 
+pragma solidity ^0.8.5;
 
-contract Bank { 
+contract Practice {
 
-address owner; 
+    address user;
+    bool online;
 
-constructor() { 
-owner = msg.sender; // The owners address that deploys the contract or initiates a transaction in the bank. 
-} 
+    constructor() {
+        user = msg.sender; /*The owners address that deploys the contract or 
+                              initiates a transaction in the bank.*/    
+        online = true;
+        
+    }
 
-mapping(address => uint) balance; // maps the owner's address to the balance uint.
+    mapping(address => uint) owner; 
 
-function deposit(uint toDeposit) public returns(uint) {
- require(msg.sender == owner); // Ensures that the person initiating the transaction is the owner.
-  balance[msg.sender] += toDeposit; // Top-up to an already existing balance so we can be able to carryout the transaction.
-   return balance[msg.sender]; 
-   } 
+    function deposit(uint toDeopsit) public returns (uint) {
+        require(msg.sender == user); /*Ensures that the person 
+                                     initiating the transaction is the owner.*/
+        require(online == true);
+        owner[msg.sender] += toDeopsit; /* Top-up to an already existing balance so 
+                                         we can be able to carryout the transaction.*/
+        return owner[msg.sender];
+    } 
 
-function transfer(address recipient, uint amount) public { 
-require(balance[msg.sender]>=amount, "Insufficient Balance"); 
-require(msg.sender != recipient, "You can't send money to yourself!");
- _transfer(msg.sender, recipient, amount); 
- } 
-function _transfer(address from, address to, uint amount) private { 
-balance[from] -= amount; //debits from owners address
-balance[to] += amount;  //credits recipient address
-}
+    function transfer(address recipient, uint amount) public {
+        require(owner[msg.sender] >= amount, "Insufficient Funds!");
+        _transfer(msg.sender, recipient, amount);
+    }
 
-function getBalance() public view returns(uint) {
- return balance[msg.sender]; // returns owners balance after the transaction have been carried out.
- } 
+    function _transfer(address from, address to, uint amount) private {
+        owner[from] -= amount; /*debits from owners address
+                                 credits recipient address*/
+        owner[to] += amount;
+    }
+
+    function getBalance() view public returns (uint) {
+        return owner[msg.sender]; /*returns owners balance after the 
+                                     transaction have been carried out.*/
+    }
 
 }
